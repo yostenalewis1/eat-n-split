321dbfc36884a00daffc928b60bd12ed91e565d6
yostenalewis1 <yostenalewis9@gmail.com>
2023-12-04 03:32:22 +0200

    app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-function App() {
-  return (
-    <div className="App">
-     <p> hello world</p>
-    </div>
-  );
-}
-
-export default App;
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
